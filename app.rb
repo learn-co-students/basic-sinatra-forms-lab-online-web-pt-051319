bba7aa709fb7db392bb6dd8ada0a37e349a274e3
scratchglory <adellakenn@gmail.com>
2019-08-13 10:37:06 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,23 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+	get '/' do
+		'Hello!'
+	end
 
+	get '/newteam' do
+		# 'Hello World!'
+		erb :newteam
+	end
 
-end
+	post '/team' do
+		@name = params[:name]
+		@coach = params[:coach]
+		@pg = params[:pg]
+		@sg = params[:sg]
+		@sf = params[:sf]
+		@pf = params[:pf]
+		@c = params[:c]
+		erb :team
+	end
+end # end of Class
